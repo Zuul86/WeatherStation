da50874f92371c710ba2b0fbca7141b8527741fb
Adam Pritzl <zuul86@gmail.com>
2021-11-26 16:31:47 -0600

    add bp sensor readings

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,8 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include "DHT.h"
-
-// Digital pin connected to the DHT sensor
-#define DHTPIN 14     
-// The MQTT topics that this device should publish/subscribe
-#define AWS_IOT_PUBLISH_TOPIC   "weatherData/pub"
-#define AWS_IOT_SUBSCRIBE_TOPIC "weatherData/sub"
+#include <SFE_BMP180.h>
+#include <Wire.h>
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
@@ -25,6 +21,7 @@ BearSSL::X509List mycert(AWS_CERT_CRT);
 BearSSL::X509List x509(AWS_CERT_CA);
 
 DHT dht(DHTPIN, DHT22);
+SFE_BMP180 pressure;
 
 void connectWiFi()
 {
@@ -43,8 +40,6 @@ void connectWiFi()
 
 void connectAWS()
 {
-  void callback(char* topic, byte* payload, unsigned int length);
-
   timeClient.begin();
   while(!timeClient.update())
   {
@@ -57,10 +52,83 @@ void connectAWS()
   net.setTrustAnchors(&x509);
 
   // Connect to the MQTT broker on the AWS endpoint we defined earlier
-  client.setServer(AWS_IOT_ENDPOINT, 8883);
+  client.setServer(AWS_IOT_ENDPOINT, AWS_PORT);
+}
 
-  // Create a message handler
-  client.setCallback(callback);
+double getPressure()
+{
+  char status;
+  double T,P,p0,a;
+
+  // You must first get a temperature measurement to perform a pressure reading.
+  
+  // Start a temperature measurement:
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
+
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Use '&T' to provide the address of T to the function.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Use '&P' to provide the address of P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          delay(status);
+          p0 = pressure.sealevel(P, 265.00);
+          return(p0);
+        }
+        else 
+        {
+          Serial.println("error retrieving pressure measurement\n");
+          return 1;
+        }
+      }
+      else 
+      {
+        Serial.println("error starting pressure measurement\n");
+        return 1;
+      }
+    }
+    else 
+    {
+      Serial.println("error retrieving temperature measurement\n");
+      return 1;
+    }
+  }
+  else 
+  {
+    Serial.println("error starting temperature measurement\n");
+    return 1;
+  }
 }
 
 void publishMessage()
@@ -69,19 +137,10 @@ void publishMessage()
   doc["time"] = timeClient.getEpochTime();
   doc["sensor_h"] = dht.readHumidity();
   doc["sensor_t"] = dht.readTemperature();
+  doc["sensor_bp"] = getPressure();
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to client
   client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (unsigned int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
 }
 
 void reconnect() {
@@ -107,9 +166,15 @@ void reconnect() {
 }
 
 
+
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  Wire.begin(13,12);
+
+  if (pressure.begin()){
+    Serial.println("BMP180 init success");
+  }
   connectWiFi();
   connectAWS();
 }
