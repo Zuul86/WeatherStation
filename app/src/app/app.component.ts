cb7fa37e3208513a19899aaeee1e63e1a3f24237
Adam Pritzl <zuul86@gmail.com>
2021-11-27 16:28:19 -0600

    get weather data

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { APIService, WeatherData } from './API.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  public weatherData: Array<WeatherDataModel> = [];
+
+  constructor(private api: APIService){
+
+  }
+
+  async ngOnInit() {
+    /* fetch restaurants when app loads */
+    const result = await this.api.ListWeatherData();
+    this.weatherData = result.items?.some ? result.items.map((x) => {
+      return {...x, 
+        time: new Date(x ? x.time * 1000: 0)
+      } as WeatherDataModel;
+    }) :  [];
+  }
+  
 }
+
+export type WeatherDataModel = {
+  time: Date;
+  sensor_bp?: number | null;
+  sensor_t?: number | null;
+  sensor_h?: number | null;
+};
