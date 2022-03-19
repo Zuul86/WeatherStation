ad2ec3b25a450b5c08da63f3f6ecfdaf71b7d278
Adam Pritzl <zuul86@gmail.com>
2022-03-18 23:52:30 -0500

    remove amplifyui

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-/* import AmplifyUIAngularModule  */
-import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,8 +22,6 @@ import { SensorDataComponent } from './features/weather-data-table/components/we
   imports: [
     BrowserModule,
     AppRoutingModule,
-    /* configure app with AmplifyUIAngularModule */
-    AmplifyUIAngularModule,
     BrowserAnimationsModule,
     MatTableModule,
     MatToolbarModule,
