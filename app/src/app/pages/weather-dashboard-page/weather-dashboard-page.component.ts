955d965a31c75582222662cabddaf76e20363944
Adam Pritzl <zuul86@gmail.com>
2022-07-01 22:31:38 -0500

    wire up the store

--- app/src/app/pages/weather-dashboard-page/weather-dashboard-page.component.ts
+++ app/src/app/pages/weather-dashboard-page/weather-dashboard-page.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { Store } from '@ngxs/store';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-weather-dashboard-page',
   templateUrl: './weather-dashboard-page.component.html',
   styleUrls: ['./weather-dashboard-page.component.scss']
 })
-export class WeatherDashboardPageComponent {
+export class WeatherDashboardPageComponent implements OnDestroy {
+  @Input() temperatureUnit: string = '';
+  private subscription: Subscription;
 
-  constructor(private store: Store) { }
-
+  constructor(private store: Store) {
+    this.subscription = this.store.select(state => state).subscribe(({ appSettings }) => {
+      this.temperatureUnit = appSettings.temperatureUnit
+    })
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
