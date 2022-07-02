beb18e0751b8d7279d64196b7fe15c829270e494
Adam Pritzl <zuul86@gmail.com>
2022-07-02 14:19:56 -0500

    clean up unused imports

--- app/src/app/features/weather-data-table/components/sensor-data/sensor-data.component.spec.ts
+++ app/src/app/features/weather-data-table/components/sensor-data/sensor-data.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
 import { WeatherDataService } from 'src/app/services/weatherdata-api.service';
 import { SensorDataComponent } from './sensor-data.component';
@@ -11,7 +13,11 @@ describe('SensorDataComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SensorDataComponent],
-      imports: [MatTableModule],
+      imports: [
+        NoopAnimationsModule,
+        MatTableModule,
+        MatPaginatorModule
+      ],
       providers: [
         {
           provide: WeatherDataService,
