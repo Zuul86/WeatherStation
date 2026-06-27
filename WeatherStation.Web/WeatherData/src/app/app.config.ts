bd44d958c529a4f2ecfc5bb484ee7be60c08b5df
Adam <zuul86@gmail.com>
2026-06-27 20:28:54 +0000

    angular init

---
+++ WeatherStation.Web/WeatherData/src/app/app.config.ts
@@ -0,0 +1,26 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, inject } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideApollo(() => {
+      const httpLink = inject(HttpLink);
+
+      return {
+        link: httpLink.create({
+          uri: '<%= endpoint %>',
+        }),
+        cache: new InMemoryCache(),
+      };
+    }),
+  ],
+};
